d45e23ecb31674f3100812ae714a11b577a8af2b
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-02 10:06:04 +0300

    Working version

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Oblik\KirbyTester;
+
+function config($name = null)
+{
+    $rootNames = array_keys(include 'kirby/config/roots.php');
+    $roots = [];
+
+    if ((empty($name) || PHP_SAPI === 'cli')) {
+        $nameEnv = getenv('KIRBY_PLUGIN');
+
+        if (is_string($nameEnv)) {
+            $name = $nameEnv;
+        }
+    }
+
+    if (is_string($name)) {
+        if (preg_match('!^[.\\/\\\]!', $name)) {
+            $dirTests = realpath($name);
+        } else {
+            $dirTests = __DIR__ . "/site/plugins/$name/tests";
+        }
+
+        $dirRoots = $dirTests . '/roots';
+    }
+
+    if (!empty($dirTests)) {
+        foreach ($rootNames as $root) {
+            $path = $dirRoots . '/' . $root;
+
+            if (file_exists($path)) {
+                $roots[$root] = $path;
+            }
+        }
+
+        $bootstrap = $dirTests . '/bootstrap.php';
+
+        if (file_exists($bootstrap)) {
+            include_once $bootstrap;
+        }
+    }
+
+    return [
+        'roots' => $roots
+    ];
+}
