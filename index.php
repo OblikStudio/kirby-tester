3b5918b61751b9ed0e1b2d02e5ef4bb8a931e5bf
Hristiyan Dodov <h.dodov@gmail.com>
2019-09-10 12:56:50 +0300

    Avoid redeclaring kirbytest()

--- index.php
+++ index.php
@@ -18,21 +18,24 @@ namespace {
      * can be used when the package is used as a site dependency.
      * @param string $input Plugin name or path to tests folder
      */
-    function kirbytest(string $input)
-    {
-        $loader = new Loader();
+    
+    if (!function_exists('kirbytest')) {
+        function kirbytest(string $input)
+        {
+            $loader = new Loader();
 
-        if (preg_match('![./\\\]!', $input)) {
-            $loader->dirTests = realpath($input);
-        } else {
-            $loader->setPluginDir($loader->dirRoot . '/site/plugins/' . $input);
-        }
+            if (preg_match('![./\\\]!', $input)) {
+                $loader->dirTests = realpath($input);
+            } else {
+                $loader->setPluginDir($loader->dirRoot . '/site/plugins/' . $input);
+            }
 
-        if ($loader->dirTests) {
-            $loader->findRoots();
-            return $loader->init();
-        } else {
-            return null;
+            if ($loader->dirTests) {
+                $loader->findRoots();
+                return $loader->init();
+            } else {
+                return null;
+            }
         }
     }
 }
